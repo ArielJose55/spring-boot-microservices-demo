e542f58c6c773560b684fc2c2f871bf73e31f19b
Ariel Arnedo <arieljosearnedocervantes@gmail.com>
2018-12-11 00:07:48 -0500

    microservices de get-word

--- src/main/java/com/microservices/api/ApiCore.java
+++ src/main/java/com/microservices/api/ApiCore.java
@@ -1,6 +1,18 @@
 package com.microservices.api;
 
-@APi
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.microservices.models.Word;
+
 public interface ApiCore {
+	
+	@RequestMapping(value = "/getStep",
+	        produces = { "application/json" }, 
+	        consumes = { "application/json" },
+	method = RequestMethod.POST)
+	ResponseEntity<?> getWord(@RequestBody Word body);
 
 }
--- src/main/java/com/microservices/api/GetWordController.java
+++ src/main/java/com/microservices/api/GetWordController.java
@@ -1,8 +1,41 @@
 package com.microservices.api;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.FluentProducerTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservices.models.Word;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
-public class GetWordController {
+public class GetWordController implements ApiCore{
+
+	
+	private final ObjectMapper objectMapper;
+	
+	private final HttpServletRequest request;
+	
+	@EndpointInject(uri="direct:get-word")
+	private FluentProducerTemplate template;
+	
+	public GetWordController(ObjectMapper objectMapper, HttpServletRequest request) {
+		super();
+		this.objectMapper = objectMapper;
+		this.request = request;
+	}
+
+
+
+	@Override
+	public ResponseEntity<?> getWord(Word body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
